ced1440fc67e7be277cc2cbdb5fbeff900f63de9
Mahbub <prof.bub@live.com>
2019-10-22 18:01:06 +0700

    add entity chapters

---
+++ backend/chapters/src/index.ts
@@ -0,0 +1,26 @@
+import { config } from "dotenv";
+import express from "express";
+import expressPinoLogger from "express-pino-logger";
+import { errors } from "graphql-response-parser";
+import find from "./find";
+import log from "./utils/log";
+
+config();
+
+export const hostname = process.env.HOST || "localhost";
+export const port = +(process.env.PORT || 4e3 + 2);
+
+const app = express();
+
+app
+  .use(expressPinoLogger({ logger: log }))
+  .get("/", find);
+
+app
+  .use((e, req, res, next) => {
+    const { message } = errors(e);
+    res.status(400).json({ message });
+  });
+
+app
+  .listen(port, hostname, () => log.info(`>>> 🌎 Open ${hostname}:${port} in your browser.`));
