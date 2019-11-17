b4f2fd6075c6fcf338c3cd5d3566d9296dacecb4
Mahbub <prof.bub@live.com>
2019-11-17 21:22:59 +0700

    update test

--- backend/_graphql/src/modules/categories/test.ts
+++ backend/_graphql/src/modules/categories/test.ts
@@ -2,10 +2,8 @@ import { createTestClient } from "apollo-server-testing";
 import gql from "graphql-tag";
 import auth from "../../constants/auth";
 import { IContext } from "../../interfaces/IContext";
-import verifyIdToken from "../../modules/auth/functions/verifyIdToken";
 import constructTestServer from "../../utils/constructTestServer";
-import { DEFAULT_LIST_RESPONSE } from "../_base/test";
-import { IAuthInput } from "../auth/interface";
+import { DEFAULT_LIST_RESPONSE, QUERY_RESPONSE } from "../_base/test";
 import { LOGIN } from "../auth/test";
 import { ICategory, ICategoryInput } from "./interface";
 
@@ -23,14 +21,7 @@ export const GET_CATEGORIES = gql`
             edges {
                 ${TYPE_CATEGORY}
             }
-            message
-            pageInfo {
-                hasNextPage
-                hasPrevPage
-                nextCursor
-                prevCursor
-            }
-            totalCount
+            ${QUERY_RESPONSE}
         }
     }
 `;
@@ -56,66 +47,62 @@ let user = { email: "" };
 let categoryId = "";
 
 beforeAll(async () => {
-  const server = constructTestServer({});
-  const { mutate } = createTestClient(server);
-  const input: IAuthInput = { email: auth.email, password: auth.password };
-  const { data } = await mutate({
-    mutation : LOGIN,
-    variables: { input },
-  });
-  token = data.login.idToken;
-  user = await verifyIdToken(token);
+  const result = await LOGIN(auth.email, auth.password);
+  user = result.user;
+  token = result.token;
 });
 
-describe("Queries", () => {
-  it('should fail because "Unauthorized"', async () => {
-    const server = constructTestServer({});
-    const { query } = createTestClient(server);
-    const { data, errors } = await query({
-      query: GET_CATEGORIES,
+describe("Category", () => {
+  describe("Queries", () => {
+    it('should fail because "Unauthorized"', async () => {
+      const server = constructTestServer({});
+      const { query } = createTestClient(server);
+      const { data, errors } = await query({
+        query: GET_CATEGORIES,
+      });
+      expect(errors[0].message).toEqual("Unauthorized");
+      expect(data.categories).toEqual(null);
     });
-    expect(errors[0].message).toEqual("Unauthorized");
-    expect(data.categories).toEqual(null);
-  });
-  it("should fetches list of categories", async () => {
-    const context: IContext = { token, user };
-    const server = constructTestServer({ context });
-    const { query } = createTestClient(server);
-    const { data, errors } = await query({
-      query: GET_CATEGORIES,
-    });
-    expect(data).toMatchObject({
-      categories: DEFAULT_LIST_RESPONSE,
-    });
-    expect(errors).toEqual(undefined);
-    if (data && data.edges) {
-      const category: ICategory = data.edges[0];
-      expect(category).toMatchObject(CATEGORY_RESPONSE);
-      categoryId = category.id;
-    }
-  });
-  it("should fetch category by id", async () => {
-    const context: IContext = { token, user };
-    const server = constructTestServer({ context });
-    const { query } = createTestClient(server);
-    const input: ICategoryInput = { id: categoryId };
-    const { data, errors } = await query({
-      query    : GET_CATEGORY_BY_ID,
-      variables: { input },
-    });
-    if (errors) {
-      expect(errors[0].extensions.response.body.message).toEqual(
-        `invalid input syntax for type uuid: ""`,
-      );
-    } else {
-      if (data && data.category) {
-        const category: Record<string, any> = data;
-        expect(data).toMatchObject({ CATEGORY_RESPONSE });
-        expect(category.id).toEqual(categoryId);
-      } else {
-        expect(data.category).toEqual(null);
-      }
+    it("should fetches list of categories", async () => {
+      const context: IContext = { token, user };
+      const server = constructTestServer({ context });
+      const { query } = createTestClient(server);
+      const { data, errors } = await query({
+        query: GET_CATEGORIES,
+      });
+      expect(data).toMatchObject({
+        categories: DEFAULT_LIST_RESPONSE,
+      });
       expect(errors).toEqual(undefined);
-    }
+      if (data && data.edges) {
+        const category: ICategory = data.edges[0];
+        expect(category).toMatchObject(CATEGORY_RESPONSE);
+        categoryId = category.id;
+      }
+    });
+    it("should fetch category by id", async () => {
+      const context: IContext = { token, user };
+      const server = constructTestServer({ context });
+      const { query } = createTestClient(server);
+      const input: ICategoryInput = { id: categoryId };
+      const { data, errors } = await query({
+        query    : GET_CATEGORY_BY_ID,
+        variables: { input },
+      });
+      if (errors) {
+        expect(errors[0].extensions.response.body.message).toEqual(
+          `invalid input syntax for type uuid: ""`,
+        );
+      } else {
+        if (data && data.category) {
+          const category: Record<string, any> = data;
+          expect(data).toMatchObject({ CATEGORY_RESPONSE });
+          expect(category.id).toEqual(categoryId);
+        } else {
+          expect(data.category).toEqual(null);
+        }
+        expect(errors).toEqual(undefined);
+      }
+    });
   });
 });
